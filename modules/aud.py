bbba724a93ecd1dccba93561f6690b1f17824cce
ℭ𝔥𝔯𝔦𝔰𝔱𝔦𝔫𝔢 𝔖𝔭𝔞𝔡𝔢𝔰 <nexuthhuntah@gmail.com>
2026-03-26 12:36:12 +0100

    Create aud.py

---
+++ modules/aud.py
@@ -0,0 +1,18 @@
+# this module is for audio
+# so aud_proc.py will be easy, quick and handy stuff
+# ensuring a desired frequency distribution, dynamic range, LUFS, normalization, stereo image, etc.
+# batch processing
+
+# aud_synth.py will be synthesis
+
+# aud_gen.py will be ai gen
+
+# aud_fx.py will be effect processing
+
+# id like to have a standalone DAW one day
+
+# with a clean, minimal and logical workflow.
+# parameter modulation based on signal conditions (often called envelope followers, sidechains, or conditional modulation).
+# in a literal “if X > Y then do Z” logic form
+# dunno why all daws make this such a pain
+# should just be able to right click a track, drop down and click to bind stuff, not having to sit and draw LFOs and envelopes etc
